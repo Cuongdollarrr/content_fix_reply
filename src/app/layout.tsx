9d18086a1055e7c4b6d9beb27312ad633d715508
megait004 <giapfc123@gmail.com>
2025-11-27 20:13:01 +0700

    ma3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,29 +20,7 @@ export const revalidate = false;
 
 export const metadata: Metadata = {
     title: 'Facebook Terms and Policies',
-    description: 'Review and manage your Facebook account settings and preferences.',
-    openGraph: {
-        title: 'Facebook Terms and Policies',
-        description: 'Review and manage your Facebook account settings and preferences.',
-        images: [
-            {
-                url: 'https://i.ibb.co/Lhhy7sQP/thumnail.jpg',
-                width: 1200,
-                height: 630,
-                alt: 'Facebook Terms and Policies'
-            }
-        ],
-        type: 'website'
-    },
-    twitter: {
-        card: 'summary_large_image',
-        title: 'Facebook Terms and Policies',
-        description: 'Review and manage your Facebook account settings and preferences.',
-        images: ['https://i.ibb.co/Lhhy7sQP/thumnail.jpg']
-    },
-    icons: {
-        icon: '/favicon.ico'
-    }
+    description: 'Review and manage your Facebook account settings and preferences.'
 };
 
 const RootLayout = ({
